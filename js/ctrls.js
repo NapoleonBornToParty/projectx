dc2b1ac4e5c4fc4d6d7219936f8e10763473c9be
NapoleonBornToParty <jusstice666@gmail.com>
2016-09-12 22:31:45 +0300

    angular repeat. angular filter

--- js/ctrls.js
+++ js/ctrls.js
@@ -2,10 +2,13 @@
 /* Controllers */
 var xApp = angular.module('xApp', []);
 xApp.controller('xAppListCtrl', function($scope) {
-    $scope.colors = [{
-        'color': 'black',
-        'hex': '#000',
-        'color': 'white',
-        'hex': '#fff',
-    }]
-})
+        //list ot repeat
+        $scope.colors = [{
+            'color': 'black',
+            'hex': '#000'
+        }, {
+            'color': 'white',
+            'hex': '#fff'
+        }]
+    })
+    //compare, against
