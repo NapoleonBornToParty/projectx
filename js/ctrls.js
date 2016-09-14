a4c036a1b334103a301557685025506163ad54fc
NapoleonBornToParty <jusstice666@gmail.com>
2016-09-14 23:13:00 +0300

    ajax angular

--- js/ctrls.js
+++ js/ctrls.js
@@ -1,14 +1,22 @@
 'use strict'
 /* Controllers */
-var xApp = angular.module('xApp', []);
-xApp.controller('xAppListCtrl', function($scope) {
-        //list ot repeat
-        $scope.colors = [{
-            'color': 'black',
-            'hex': '#000'
-        }, {
-            'color': 'white',
-            'hex': '#fff'
-        }]
-    })
-    //compare, against
+var xApp = angular.module('xApp',  ['ngRoute']);
+xApp.controller('xAppMainCtrl', function($scope) {
+    $scope.alert = {
+        'search': 'Nothing there...'
+    }
+})
+xApp.controller('xAppListCtrl', function($scope, $http) {
+      $http.get('files/colors.json').then(function(response) {
+      $scope.content = response.data;
+      $scope.statuscode = response.status;
+      $scope.statustext = response.statusText;
+    });
+    $scope.colors = [{
+        'color': 'black',
+        'hex': '#000'
+    }, {
+        'color': 'white',
+        'hex': '#fff'
+    }]
+});
